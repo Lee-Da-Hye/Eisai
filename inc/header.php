77f30c86c1626f5724c80895f475b967d8b10e1a
Lee-Da-Hye <dlekgp6521@naver.com>
2023-03-08 17:09:16 +0900

    php

--- inc/header.php
+++ inc/header.php
@@ -1,3 +1,6 @@
+<?php
+
+?>
 <!DOCTYPE html>
 <html lang="ko">
 <head>
@@ -12,7 +15,7 @@
     <link rel="stylesheet" href="./style/reset.css">
     <link rel="stylesheet" href="./style/common.css">
     <link rel="stylesheet" href="./style/index.css">
-    <title>한국에자이</title>   
+    <title><?=$title;?></title>   
 </head>
 <body>
     <header id="navBar">
@@ -207,169 +210,3 @@
             </div>
         </div>
     </header>
-    <div class="main">
-        <section class="section01" id="section01">
-            <div  class="slide slide_wrap">
-                <div class="slide_item item1">
-                    <div class="content-inner ">
-                        <img src="./images/main/mv1.jpg" alt="메인이미지첫번째">
-                        <div class="content">
-                            <h1>We make Medicines</h1>
-                            <p>한국에자이는 우리 모두의 건강한 삶을 위해 우수의약품 개발과 보급에<br> 최선을 다하고 있습니다.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="slide_item item2">
-                    <div class="content-inner">
-                        <img src="./images/main/mv2.jpg" alt="메인이미지두번째">
-                        <div class="content">
-                            <h1>We make Solutions</h1>
-                            <p>한국에자이는 우리가 안심하고 살 수 있는 사회를 위해 다양한 헬스케어<br> 솔루션을 함께 만들어갑니다.</p>
-                        </div>
-                    </div>
-                </div>
-                <ul class="slide_pagination">
-                </ul>
-            </div>
-        </section>
-        <section class="section02 fadeIn" id="section02">
-            <div class="sec02 layout-marginAuto">
-                <div class="sec02-inner ">
-                    <h1>Think Patients First</h1>
-                    <p>Eisai는 전문의약품에서 일반의약품에 이르기까지 끊임없는 연구개발을 통해 <br>인간에게 희망을 심어 주는 좋은약 만들기에 앞장서고 있습니다.</p>
-                    <p><a href="./product.html">제품소개 바로가기</a></p>
-                    <div class="sec02-img">
-                        <img src="./images/main/mc2_img.png" alt="제품이미지">
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section class="section03 fadeIn" id="section03">
-            <div class="sec03-inner">
-                <div class="sec03-inner1">
-                    <h3>human health care</h3>
-                    <p>한국에자이는 가치있는 의약품과 서비스의 제공을 통해 인류의 건강한 삶에 이바지합니다.</p>
-                    <p><a href="./what_hhc.html">view more</a></p>
-                </div>
-                <div class="sec03-img"></div>
-            </div>
-        </section>
-        <section class="section04 fadeIn" id="section04">
-            <div class="sec04-inner layout-marginAuto">
-                <h3>Innovation For the Better Life</h3>
-                <p>환자와 가족들을 최우선적으로 생각하면서 헬스케어 분야에서 사회적 가치를 창출합니다.</p>
-                <p><a href="./innovation.html">기업사회 혁신 바로가기</a></p>
-            </div>
-        </section>
-        <section class="section05 fadeIn" id="section05">
-            <div class="sec05-inner layout-marginAuto">
-                <div class="sec05-inner1">
-                    <div class="sec05-more">
-                        <span>최신소식</span>
-                        <span>view more +</span>
-                    </div>
-                    <article>
-                        <ul class="article">
-                            <li>
-                                <a href="#">
-                                    <strong>한국에자이, 기업사회혁신 활동 본격화 = 성미산 마을돌봄 리...</strong>
-                                    <p>[라포르시안] 한국에자이가 지역사회 돌봄역량 강화를 통해 돌봄사회로의 전환을<br>모색하는'성미산 마을돌봄 리빙랩'에 참여해 기업사회혁신 활동을...</p>
-                                    <small>2022.09.27</small>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <strong>롯데면세점-한국에자이,MOU체결</strong>
-                                    <p>롯데면세점-한국에자이,MOU체결..."건강관리 앱 '헬피'통해 홈케어족<br>잡는다" [바이오타임즈]롯데면세점(대표 이갑)이 한국에자이의 건강 관리 앱인...</p>
-                                    <small>2022.06.21</small>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <strong>한국에자이, 뇌 건강 인지 플랫폼 카페 '판교를 IT多' 개관</strong>
-                                    <p>글로벌 헬스케어 기업 한국에자이는 경기 성남 분당구 소재 판교종합사회복지관에 뇌<br>건강을 위한 인지 플랫폼 카페 '판교를 IT多(잇다)를 15일 개관한다고 12일 밝혔다. 이날...</p>
-                                    <small>2022.04.12</small>
-                                </a>
-                            </li>
-                        </ul>
-                    </article>
-                </div>
-                <div class="sec05-img">
-                    <p>채용</p>
-                    <p>한국에자이는 여러분의 의미있는 도전을 기다립니다.</p>
-                </div>
-            </div>
-        </section>
-    </div>
-
-    <aside>
-        <div class="fixed top animate__animated">
-            <ul>
-                <li><a href="#section01" class="active"></a></li>
-                <li><a href="#section02"></a></li>
-                <li><a href="#section03"></a></li>
-                <li><a href="#section04"></a></li>
-                <li><a href="#section05"></a></li>
-            </ul>
-        </div>
-    </aside>
-
-    <footer>
-        <div class="footer-top layout-marginAuto">
-            <ul class="footer-menu ">
-                <li>
-                    <a href="#">
-                        <img src="./images/main/footer_menu1.png" alt="브로슈어">
-                        <span>한국에자이 브로슈어</span>
-                    </a>
-                </li>
-                <li><a href="#">
-                    <img src="./images/main/footer_menu2.png" alt="공식포스트">
-                        <span>한국에자이 공식포스트</span>
-                </a></li>
-                <li><a href="#">
-                    <img src="./images/main/footer_menu3.png" alt="건강케어">
-                    <span>HeLpy</span>
-                    <span class="small">맘편한 건강케어</span>
-                </a></li>
-                <li><a href="#">
-                    <img src="./images/main/footer_menu4.png" alt="헬스케어">
-                    <span>ERICCHI</span>
-                    <span class="small">시니어 헬스케어의 든든한 친구</span>
-                </a></li>
-                <li><a href="#">
-                    <img src="./images/main/footer_menu5.png" alt="홍보영상">
-                    <span>홍보영상</span>
-                </a></li>
-            </ul>
-        </div>
-        <div class="footerBottom-back">
-            <div class="footer-bottom layout-marginAuto">
-                <div class="footer-logo">
-                    <img src="./images/common/logo_footer.png" alt="logo">
-                </div>
-                <address>
-                    <span>서울시 강남구 봉은사로 86길 6 빌딩 레베쌍트 10층/</span>
-                    <span>TEL : 02-3451-5500/</span>
-                    <span>FAX : 02-3451-5599<br></span>
-                    <span>10F Revessant, 6, Bongeunsa-ro 86-gil, Gangnam-gu, Seoul, 135-878, Korea/</span>
-                    <span>TEL : 02-3451-5500/</span>
-                    <span>FAX : 02-3451-5599</span>
-                </address>
-                <div class="footer-copyright">
-                    <p class="right">개인정보처리방침</p>
-                    <p>Copyright © EISAI KOREA INC All Right Reserved.</p>
-                </div>
-            </div>
-        </div>
-    </footer>
-    
-    <script src="./script/jquery-3.6.1.js"></script>
-    <script src="./script/jquery-ui.js"></script>
-    <script src="./script/index.js"></script>
-    <script src="./script/slide.js"></script>
-    <script src="./script/common.js"></script>
-    <script src="./script/fadeIn.js"></script>
-    
-</body>
-</html>
