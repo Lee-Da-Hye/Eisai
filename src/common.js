0e18f2018faeeb2f786c6d38297fb62619f3c3e4
Lee-Da-Hye <dlekgp6521@naver.com>
2023-03-09 19:25:46 +0900

    dd

--- src/common.js
+++ src/common.js
@@ -1,13 +1,18 @@
+const navBar = document.querySelector('#navBar');
+const logoImg1 = document.querySelector('.logoColor');
+const logoImg2 = document.querySelector('.logoWhite');
 $(window).on('scroll', ()=>{
-  let navbar = $('#navbar');
-  console.log(navbar);
+
   if(window.scrollY > 600){
-    navbar.addClass('scroll');
-    navbar.css('backgroundColor', 'white');
-    
+    navBar.addClass('scroll');
+    navBar.css('backgroundColor', 'white');
+    logoImg1.css('display', 'none');
+    logoImg2.css('display', 'block');
   }else{
-    navbar.removeClass('scroll');
-    navbar.css('backgroundColor', 'trasnparent');
+    navBar.removeClass('scroll');
+    navBar.css('backgroundColor', 'trasnparent');
+    logoImg1.css('display', 'block');
+    logoImg2.css('display', 'none');
   }
 })
 
