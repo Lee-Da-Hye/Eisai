fce7b4f79defd2c5db335be3165df67420165bd2
Lee-Da-Hye <dlekgp6521@naver.com>
2023-03-09 19:38:49 +0900

    ddd

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
 />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-
+<script type="text/javascript" src="/src/common.js?<?=time()?>"></script>
 <script type="text/javascript" src="/src/fadeIn.js?<?=time()?>"></script>
 </head>
 <body>
@@ -51,6 +51,6 @@
     ?>
 </footer>
 <!--/하단 영역-->
-<script type="text/javascript" src="/src/common.js?<?=time()?>"></script>
+
 </body>
 </html>
--- src/common.js
+++ src/common.js
@@ -1,18 +1,32 @@
-const navBar = document.querySelector('#navBar');
+$(document).ready(function() {
+const navBar = document.getElementById('navBar');
 const logoImg1 = document.querySelector('.logoColor');
 const logoImg2 = document.querySelector('.logoWhite');
-$(window).on('scroll', ()=>{
+const ham = document.querySelector('.ham-btn');
+const hamSpan = document.querySelectorAll( '.ham-btn > span' );
+/*const hamSpan1 = document.querySelector('.hamTop');
+const hamSpan2 = document.querySelector('.hamMid');
+const hamSpan3 = document.querySelector('.hamBot');*/
 
-  if(window.scrollY > 600){
-    navBar.addClass('scroll');
-    navBar.css('backgroundColor', 'white');
-    logoImg1.css('display', 'none');
-    logoImg2.css('display', 'block');
-  }else{
-    navBar.removeClass('scroll');
-    navBar.css('backgroundColor', 'trasnparent');
-    logoImg1.css('display', 'block');
-    logoImg2.css('display', 'none');
+
+window.onscroll = function() {
+  "use strict";
+  if (document.body.scrollTop >= 600 || document.documentElement.scrollTop >=600) {
+    navBar.classList.add('scroll');
+    navBar.style.backgroundColor = 'white';
+    logoImg2.style.display = 'block';
+    logoImg1.style.display = 'none';
+    hamSpan[0].style.backgroundColor = 'black';
+    hamSpan[1].style.backgroundColor = 'black';
+    hamSpan[2].style.backgroundColor = 'black';
+  } else {
+    navBar.classList.remove('scroll');
+    navBar.style.backgroundColor = 'transparent';
+    logoImg1.style.display = 'block';
+    logoImg2.style.display = 'none';
+    hamSpan[0].style.backgroundColor = 'white';
+    hamSpan[1].style.backgroundColor = 'white';
+    hamSpan[2].style.backgroundColor = 'white';
   }
-})
-
+};
+});
