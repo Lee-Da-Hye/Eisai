449e681ab3235976df43c764ff293a28e9fd3bfc
Lee-Da-Hye <dlekgp6521@naver.com>
2023-03-09 17:16:57 +0900

    dd

--- index.php
+++ index.php
@@ -21,7 +21,6 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
 />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.13.2/jquery-ui.min.js"></script>
 <script type="text/javascript" src="/src/common.js?<?=time()?>"></script>
 <script type="text/javascript" src="/src/fadeIn.js?<?=time()?>"></script>
 </head>
